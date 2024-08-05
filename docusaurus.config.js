47c4e6d600b71c4a6afc08bb371ccc90feadb3c8
Liudmila Nemkova <lnemkova@adapty.io>
2024-08-05 14:57:34 +0200

    trailing slash added

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/docs-test/',
-
+  trailingSlash: true,
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'adapty', // Usually your GitHub org/user name.
