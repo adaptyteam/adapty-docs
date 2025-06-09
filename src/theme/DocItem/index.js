28f7c7b9da42b2c08c26f994ba8705760304cb13
eandreeva-twr <eandreeva@adapty.io>
2025-06-09 12:26:35 +0200

    Update handling metadata

--- src/theme/DocItem/index.js
+++ src/theme/DocItem/index.js
@@ -1,11 +1,18 @@
 // src/theme/DocItem/index.js
 import React from 'react';
 import OriginalDocItem from '@theme-original/DocItem';
+import { useDoc } from '@docusaurus/theme-common/internal';
 
 export default function DocItemWrapper(props) {
   const { content: DocContent } = props;
   const { frontMatter } = DocContent;
   const whatsNewLinks = frontMatter['whats_new'] || [];
+  const { metadata } = useDoc();
+
+  // Add weight to metadata if it exists in frontmatter
+  if (frontMatter.weight) {
+    metadata.weight = frontMatter.weight;
+  }
 
   return <OriginalDocItem {...props} footerProps={{ links: whatsNewLinks }} />;
 }
