9a416224f9aad76901228832b4a9b8f27784131f
eandreeva-twr <eandreeva@adapty.io>
2025-06-09 12:42:13 +0200

    Add rank

--- src/theme/DocItem/index.js
+++ src/theme/DocItem/index.js
@@ -1,11 +1,23 @@
 // src/theme/DocItem/index.js
 import React from 'react';
 import OriginalDocItem from '@theme-original/DocItem';
+import { Helmet } from 'react-helmet';
 
 export default function DocItemWrapper(props) {
   const { content: DocContent } = props;
   const { frontMatter } = DocContent;
   const whatsNewLinks = frontMatter['whats_new'] || [];
+  const rank = frontMatter.rank;
 
-  return <OriginalDocItem {...props} footerProps={{ links: whatsNewLinks }} />;
+  return (
+    <>
+      {/* Inject rank as a meta tag if it exists */}
+      {rank !== undefined && (
+        <Helmet>
+          <meta name="rank" content={String(rank)} />
+        </Helmet>
+      )}
+      <OriginalDocItem {...props} footerProps={{ links: whatsNewLinks }} />
+    </>
+  );
 }
